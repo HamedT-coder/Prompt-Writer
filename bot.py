31abd646086521d37667cca2c1a2977931d068b9
HamedT-coder <shtahamip@gmail.com>
2025-12-22 20:57:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,20 +89,6 @@ async def error_handler(update, context):
     logger.exception("Unhandled error", exc_info=context.error)
     
 # ================= FAKE SERVER =================
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class HealthHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"OK")
-
-def start_fake_server():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), HealthHandler)
-    server.serve_forever()
-
-threading.Thread(target=start_fake_server, daemon=True).start()
 
 def main():
     # ✅ اول پورت رو باز کن (خیلی مهم)
