364d7fa5708dee96e80d460217d116b94377d6cf
HamedT-coder <shtahamip@gmail.com>
2025-12-23 10:55:03 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,9 +114,7 @@ def main():
     application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
     )
-
     application.add_error_handler(error_handler)
-
     application.run_polling()
 
 if __name__ == "__main__":
